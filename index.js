2b984871285081563b0a55f69859c721eef1858b
ppypenko <ppypenko@student.neumont.edu>
2016-04-11 15:59:57 -0600

    Added session and return buttons

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 "use strict";
-var express = require('express')
-    , bodyParser = require('body-parser')
-    , expressSession = require('express-session')
-    , app = express()
-    , jade = require('jade')
-    , path = require('path')
-    , hasher = require('./hash.js')
-    , urlParser = bodyParser.urlencoded({
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    expressSession = require('express-session'),
+    app = express(),
+    jade = require('jade'),
+    path = require('path'),
+    hasher = require('./hash.js'),
+    urlParser = bodyParser.urlencoded({
         extended: false
     });
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({
+    secret: "S3Cr3TM3zz3G3",
+    saveUninitialized: true,
+    resave: true
+}));
+var accessChecker = function (req, res, next) {
+    if (req.session.user && req.session.user.isAuthenticated) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
 
 // viewed at http://localhost:8080
 app.get('/', function (req, res) {
@@ -22,16 +34,26 @@ app.get('/login', function (req, res) {
     res.sendFile(path.join(__dirname + '/login.html'));
 });
 app.post('/login', urlParser, function (req, res) {
-    if (hasher.verify(req.body.login, req.body.password)) {
+    var user = req.body.login;
+    if (hasher.verify(user, req.body.password)) {
+        req.session.user = {
+            isAuthenticated: true,
+            username: user
+        };
         res.redirect('/admin');
     } else {
         res.sendFile(path.join(__dirname + '/login.html'));
     }
 });
 // viewed at http://localhost:8080/admin
-app.get('/admin', function (req, res) {
+app.get('/admin', accessChecker, function (req, res) {
     res.render('admin', {
         rvsp: hasher.rvspInfo()
+    });
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+        }
     });
 });
 // viewed at http://localhost:8080/location
