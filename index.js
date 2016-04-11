45c2f7222c69e954fff90e92d510965d90c9bef2
ppypenko <ppypenko@student.neumont.edu>
2016-04-11 07:02:32 -0600

    Have jade working.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     expressSession = require('express-session'),
     app = express(),
+    jade = require('jade'),
     path = require('path'),
-    //    hasher = require('hash.js'),   
+    hasher = require('./hash.js'),   
     urlParser = bodyParser.urlencoded({
         extended: false
     });
-
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // viewed at http://localhost:8080
@@ -19,16 +21,16 @@ app.get('/', function (req, res) {
 app.get('/login', function (req, res) {
     res.sendFile(path.join(__dirname + '/login.html'));
 });
-//app.post('/login', urlParser, function (req, res) {
-//    if (hasher.verify(req.body.login, req.body.password)) {
-//        res.redirect('/admin');
-//    } else {
-//        res.sendFile(path.join(__dirname + '/login.html'));
-//    }
-//});
+app.post('/login', urlParser, function (req, res) {
+    if (hasher.verify(req.body.login, req.body.password)) {
+        res.redirect('/admin');
+    } else {
+        res.sendFile(path.join(__dirname + '/login.html'));
+    }
+});
 // viewed at http://localhost:8080/admin
 app.get('/admin', function (req, res) {
-    res.sendFile(path.join(__dirname + '/admin.html'));
+    res.render('admin', {test: hasher.rvspInfo()});
 });
 // viewed at http://localhost:8080/location
 app.get('/location', function (req, res) {
